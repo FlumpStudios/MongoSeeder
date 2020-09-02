ac02e7cbbbeb4e1e860011a94e9a72e03ac70c0a
Paul Marrable <Paul.Marrable@codemasters.com>
2020-09-02 10:45:26 +0100

    Small tidy up

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using MongoDB.Bson.Serialization;
 using MongoDB.Driver;
 using System;
+using System.ComponentModel;
 using System.Threading.Tasks;
 using System.Windows;
 
@@ -13,12 +14,12 @@ namespace MongoSeeder
     public partial class MainWindow : Window
     {
         private IMongoCollection<BsonDocument> _collection;
-        
+
         public MainWindow()
         {
             InitializeComponent();
         }
-        
+
         private void UpdateDbDetails()
         {
             string connectionString = ConnectionStringBox.Text;
@@ -56,17 +57,34 @@ namespace MongoSeeder
              await _collection.InsertOneAsync(
                 obj);
 
-        private string ReplaceValues(string t)        
+        private string ReplaceValues(string t)
         {
+            //TODO: Move values to a config file
+            const int STRING_SIZE = 8;
+            Range range = new Range(1, 1000);
+            Random rnd = new Random();
+            
+
             const string STRING_VAL = "{{string}}";
             const string INT_VAL = "{{number}}";
-            
-            Random rnd = new Random();
 
-            t = t.Replace(STRING_VAL, Guid.NewGuid().ToString(),StringComparison.OrdinalIgnoreCase);
-            t = t.Replace(INT_VAL, rnd.Next(1,100).ToString(), StringComparison.OrdinalIgnoreCase);
+            
+            t = t.Replace(STRING_VAL, Guid.NewGuid().ToString().Substring(STRING_SIZE), StringComparison.OrdinalIgnoreCase);
+            t = t.Replace(INT_VAL, rnd.Next(range.min, range.max).ToString(), StringComparison.OrdinalIgnoreCase);
 
             return t.Trim();
-        }   
+        }
+    }
+    
+    public struct Range
+    {
+        public int min;
+        public int max;
+
+        public Range(int min_val, int max_val)
+        {
+            min = min_val;
+            max = max_val;
+        }
     }
 }
